ae316bd7e8ae93491ec4bab887cc1a80d9aeebf1
Anıl Gültekin <117902946+anillgultekinn@users.noreply.github.com>
2024-07-11 16:20:33 +0300

    Added Pagination

--- src/pages/WorkHour/WorkHour.tsx
+++ src/pages/WorkHour/WorkHour.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import "./WorkHour.css"
 import SideProfileMenu from '../../components/SideProfileMenu/SideProfileMenu'
-import { Button, Col, Modal, Row } from 'react-bootstrap';
+import { Button, Col, Modal, Pagination, Row } from 'react-bootstrap';
 import { Formik, Form } from 'formik';
 import TextInput from '../../utilities/customFormControls/textInput';
 import AddWorkHourRequest from '../../models/requests/workHour/addWorkHourRequest';
@@ -10,10 +10,11 @@ import workHourService from '../../services/workHourService';
 import "react-datepicker/dist/react-datepicker.css";
 import { toast } from 'react-toastify';
 import GetListWorkHourResponse from '../../models/responses/workHour/getListWorkHourResponse';
+import { Paginate } from '../../models/paginate';
 
 export default function WorkHour() {
 
-    const [workHours, setWorkHours] = useState<GetListWorkHourResponse[]>([]);
+    const [workHours, setWorkHours] = useState<Paginate<GetListWorkHourResponse>>();
 
     const [show, setShow] = useState(false);
     const user = authService.getUserInfo();
@@ -22,20 +23,21 @@ export default function WorkHour() {
     const handleShow = () => setShow(true);
     const userId = user?.id;
 
+    const [pageIndexState, setPageIndexState] = useState<any>(0)
+
 
     useEffect(() => {
         getWorkHourByAccountId();
-    }, [])
+    }, [pageIndexState, userId])
 
 
     const getWorkHourByAccountId = () => {
         if (userId) {
             workHourService.getByAccountId(userId, 0, 10).then(result => {
-                setWorkHours(result.data.items);
+                setWorkHours(result.data);
             })
         }
     }
-
 
     // useEffect(() => {
     //     workHourService.getByAccountId(user.id, 0, 15).then(result => {
@@ -76,6 +78,36 @@ export default function WorkHour() {
 
         const formattedDate = `${day}-${month}-${year}`;
         return formattedDate;
+    }
+
+    useEffect(() => {
+        if (pageIndexState !== undefined) {
+            workHourService.getByAccountId(userId, pageIndexState, 10).then(result => {
+                setWorkHours(result.data);
+            });
+        }
+    }, [pageIndexState]);
+
+    function changePageIndex(pageIndex: any) {
+        setPageIndexState(pageIndex);
+    }
+
+    const pages = [];
+
+    if (workHours?.pages)
+        for (let pageIndex = 0; pageIndex < workHours?.pages; pageIndex++) {
+            pages.push(
+                <Pagination.Item onClick={() => changePageIndex(pageIndex)} key={pageIndex} active={pageIndex === pageIndexState}> {pageIndex + 1} </Pagination.Item>
+            );
+        }
+
+
+    function calculateWorkHours(startHour: string, endHour: string) {
+        const start = new Date(`2000-01-01T${startHour}`);
+        const end = new Date(`2000-01-01T${endHour}`);
+        const diffMs = end.getTime() - start.getTime();
+        const diffHours = diffMs / (1000 * 60 * 60);
+        return `${diffHours.toFixed(2)} saat`;
     }
 
 
@@ -165,25 +197,52 @@ export default function WorkHour() {
                                 <th>Giriş Saati</th>
                                 <th>Çıkış Saati</th>
                                 <th>Çalışılan Tarih</th>
+                                <th>Çalışılan Saat</th>
                             </tr>
                         </thead>
                         <tbody>
-                            {workHours.map((workHour: any) => (
+                            {workHours?.items.map((workHour: any) => (
                                 <tr >
                                     <td data-label="firstName">{workHour.firstName}</td>
                                     <td data-label="firstName">{workHour.lastName}</td>
                                     <td data-label="startHour">{workHour.startHour}</td>
                                     <td data-label="endHour">{workHour.endHour}</td>
                                     <td data-label="email">{formatDate(workHour.studyDate)}</td>
+                                    <td>{calculateWorkHours(workHour.startHour, workHour.endHour)}</td>
                                 </tr>
                             ))}
+                        </tbody>
 
+                        <tbody>
+                            <tr>
+                                <td colSpan={5}><strong>Toplam Çalışılan Saatler</strong></td>
+                                <td colSpan={5}>
+                                    {workHours?.items.reduce((totalHours, workHour) => {
+                                        const start = new Date(`2000-01-01T${workHour.startHour}`);
+                                        const end = new Date(`2000-01-01T${workHour.endHour}`);
+                                        const diffMs = end.getTime() - start.getTime();
+                                        const diffHours = diffMs / (1000 * 60 * 60);
+                                        return totalHours + diffHours;
+                                    }, 0).toFixed(2)} saat
+                                </td>
+                            </tr>
                         </tbody>
                     </table>
 
 
+                    <Col className="pagination-area" md={12}>
+                        <Pagination style={{ display: workHours && workHours.pages > 1 ? 'flex' : 'none' }}>
+                            <Pagination.Prev className="pagination-prev-next" disabled={pageIndexState === 0} onClick={() => changePageIndex(pageIndexState - 1)} >
+                                <span aria-hidden="true"> &lt; </span>
+                            </Pagination.Prev>
+                            {pages}
+                            <Pagination.Next className="pagination-prev-next" disabled={pageIndexState + 1 === workHours?.pages} onClick={() => changePageIndex(pageIndexState + 1)}>
+                                <span aria-hidden="true"> &gt; </span>
+                            </Pagination.Next>
+                        </Pagination>
+                    </Col>
                 </div>
             </div>
-        </div>
+        </div >
     )
 }
